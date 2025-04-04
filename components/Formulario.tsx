c76c9020a963cec336ea50112c1c96c93dd46e35
Pablo B. A. <pablo.borderas@gmail.com>
2025-04-04 16:15:32 +0200

    Index Hecho

---
+++ components/Formulario.tsx
@@ -0,0 +1,23 @@
+
+type data = {
+    phone: string,
+    country: string,
+    is_valid: boolean
+}
+
+export default function Formulario(props:data) {
+    const phone = props.phone
+    const country = props.country
+    const is_valid = props.is_valid
+    return (
+    <>
+    <form method="GET" action="/" >
+    <input name="phone" type="text" placeholder="telefono" required/>
+    <button type="submit">Enviar</button>
+    </form>
+    {!is_valid && <h4 style={{color: "red"}}>Telefono no valido</h4>}
+    {is_valid && <h4>Telefono: {phone}</h4>}
+    {is_valid && <h4>Pais: <a href="./">{country}</a></h4>}
+    </>
+    )
+}
--- routes/index.tsx
+++ routes/index.tsx
@@ -1,8 +1,35 @@
+import { FreshContext, PageProps } from "$fresh/server.ts";
+import Formulario from "../components/Formulario.tsx";
+import { Handlers } from "$fresh/server.ts";
 
-export default function Home() {
+type data = {
+  format_international: string,
+  country: string,
+  is_valid: boolean
+}
+
+export const handler:Handlers<data> = {
+    GET: async (req:Request, ctx:FreshContext<unknown,data>) => {
+      const url = new URL(req.url)
+      const phone = url.searchParams.get("phone")|| undefined
+      if(phone === undefined) return ctx.render({format_international: "",country: "",is_valid: false})
+      const apiKey = Deno.env.get("API_KEY")
+      if(!apiKey) throw new Error("Api key not found")
+      const urlApi = "https://api.api-ninjas.com/v1/validatephone?number="+phone
+      const data = await fetch(urlApi,{
+        headers: {
+          'X-Api-Key': apiKey
+        }
+      })
+      if(data.status !== 200) throw new Error("Error en la respuesta de la api Phone")
+      const response = await data.json()
+      return ctx.render({format_international: response.format_international,country: response.country,is_valid: response.is_valid})
+    }
+}
+
+export default function Home(props:PageProps<data>) {
+  const {format_international,country,is_valid} = props.data
   return (
-    <div>
-      <h1>Hola Mundo, por cuarta vez</h1>
-    </div>
+    <Formulario phone={format_international} country={country} is_valid={is_valid}/>
   );
 }
